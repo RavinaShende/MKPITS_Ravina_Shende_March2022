76125f06a1faf2c1047cab0481410c1f2bbf96c8
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-18 11:07:24 +0530

    area of rectangle

---
+++ as-7 area of rectangle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int h=7, w=5, a, b, p;
+	a=h*w;
+	b=h+w;
+	p=2*b;
+	printf("Area of rectangle %d",a);
+	printf("\nPerimeter of rectangle %d",p);
+}
