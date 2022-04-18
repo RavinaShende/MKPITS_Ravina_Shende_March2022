3e081295cd4250ed22a90a60a09d824e5a857a3c
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-18 14:29:54 +0530

    result of dividing of 2nos

---
+++ as-5 div by two nos.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num1, num2, res;
+	printf("enter any two number");
+	scanf("%d%d", &num1, &num2);
+	res=num1/num2;
+	printf("result of dividing two number %d", res);
+}
