f0ef2d391f7fe9b8e125cd8dd55ba6ea6955e85a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-18 12:11:09 +0530

    rev. of xml

---
+++ as-6 rev.of xml.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	
+	char a,b,c,d;
+	printf("Test charactors : 'X''M''L'");
+	a='x';
+	b='M';
+	c='L'; 
+	printf("The reverse of XML is %c%c%c",c,b,a);
+}
