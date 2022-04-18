0d95387bafff4fb442cfe11a8b73fe309bb8cefe
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-18 12:13:15 +0530

    sum of 2int.

---
+++ as-10 sum of 2int.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num1, num2, res;
+	printf("Enter any integer number");
+	scanf("%d%d",&num1,&num2);
+	res=num1+num2;
+	printf("sum of two integer number%d", res);
+}
