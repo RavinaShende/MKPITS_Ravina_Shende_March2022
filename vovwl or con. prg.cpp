0743a2c6c533e6407aaa868fadc10714eafc0db7
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-20 14:24:29 +0530

    vowel or consonant prg.

---
+++ vovwl or con. prg.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	char choice;
+	printf("enter any character");
+	scanf("%c", &choice);
+	if(choice=='a')
+	{
+		printf("Entered character is vowel");
+	}else
+	if(choice=='e')
+	{
+		printf("Entered character is vowel");
+	}else
+	if(choice=='i')
+	{
+		printf("Entered character is vowel");
+	}else
+	if(choice=='o')
+	{
+		printf("Entered character is vowel");
+	}else
+	if(choice=='u')
+	{
+		printf("Entered character is vowel");
+	}else
+	{
+		printf("Entered character is consonant");
+	}
+		
+	}
