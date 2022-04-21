6a722f4f830b5ac6c73eeaf963a95a8aa62dd434
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-21 11:35:07 +0530

    display even no from 1to10

---
+++ loop_even no 1to10.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i=1;
+	printf("Even number from 1 to 10:");
+	while(i<=10)
+	{
+		if(i%2==0)
+		{
+			printf("\n%d",i);
+		}
+		i++;
+	}
+    printf("\n");
+}
