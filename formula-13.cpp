6955efb385bb7b5457468e3cf12604b5d9007db5
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-21 13:08:04 +0530

    assignment on formula

---
+++ formula-13.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,c;
+	printf("value of a");
+	scanf("%d",&a);
+	printf("value of b");
+	scanf("%d",&b);
+	c=(2*a)-b;
+	printf("value of c= %d",c);
+}
