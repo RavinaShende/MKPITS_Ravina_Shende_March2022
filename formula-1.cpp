20a8f5cb5343f7fffb3d6c1590b9a6923586c8ad
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-21 12:56:08 +0530

    assignment on formula

---
+++ formula-1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int w, l, a;
+	printf("Length of rectangle");
+	scanf("%d",&l);
+	printf("Width of rectangle");
+	scanf("%d",&w);
+	a=l*w;
+	printf("Area of Rectangle is %d",a);
+}
