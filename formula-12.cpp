f70cea76dcaf60b45fa3c62ad535582f75622f90
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-21 13:08:37 +0530

    assignment on formula

---
+++ formula-12.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float pi=3.14, w, f;
+	printf("put the value of angular velocity");
+	scanf("%f",&w);
+    f=w/2*pi;
+	printf("Radius, r= %f",f);
+}
