e4168aabc29ebe0c61ab1add41fe2a88708189fb
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-21 12:43:37 +0530

    display 10 to 1 nos addition

---
+++ loop_add10to1no.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i=10,n=0;
+	printf("addition of 10 to 1 nos:");
+	while(i>=1)
+	{
+		if(n=n+i)
+		{
+		printf("\n%d",n);
+    	}
+    	i--;
+	}
+	
+}
