2297514e64572aea3156ec474dfa6b0e1730ed41
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-04-21 12:56:36 +0530

    assignment on formula

---
+++ formula-2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int m, a, f;
+	printf("enter the value of mass");
+	scanf("%d",&m);
+	printf("enter the value of accelration");
+	scanf("%d",&a);
+    f=m*a;
+	printf("Force is %d",f);
+}
