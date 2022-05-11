baa246d4bdf640b81d46e8183ae531e6b300ad0d
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-05-11 17:02:55 +0530

    enumeration program

---
+++ enum1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+enum wday{sun,mon,thue,wed,thr,fri,sat};
+main()
+{
+	//enum wday{sun,mon,thue,wed,thr,fri,sat};
+	enum wday day;
+	day=fri;
+	printf("%d",day);
+}
