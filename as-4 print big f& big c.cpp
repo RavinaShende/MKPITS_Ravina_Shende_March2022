1e7d9c1d4289ad068e3648ab879240f092bb5d3b
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-05-15 16:24:23 +0530

    Program on print big 'F' & big 'C'

---
+++ as-4 print big f& big c.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf("#######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("     ######\n");
+	printf("  ##        ##\n");
+	printf(" #\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf(" #\n");
+	printf("  ##        ##\n");
+	printf("     ######");
+}
