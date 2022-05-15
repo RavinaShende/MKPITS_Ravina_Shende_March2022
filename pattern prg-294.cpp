8858bc595df31a84bbab1c85875b5865dd4e0b7a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-05-16 00:45:51 +0530

    To print - 55555,4444,333,..

---
+++ pattern prg-294.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int r,c,sp;
+	for (r=5;r>=1;r--)
+	{
+		for(sp=51;sp<=51-r;sp++)
+		{
+			printf(" ");
+		}
+		for (c=1;c<=r;c++)
+		{
+			printf("%d",r);
+		}
+		printf("\n");
+	}
+}
