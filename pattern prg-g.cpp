46dec0957a0028a474eb5634ac87157a62d9bfd8
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-05-17 10:56:14 +0530

    To print 1,321,54321,...

---
+++ pattern prg-g.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int r, c, sp;
+	for (r=1;r<=7;r=r+2)
+	{
+		for (sp=1;sp<=41-r;sp++)
+		{
+			printf(" ");
+		}
+		for (c=r;c>=1;c--)
+		{
+			printf("%d ",c);
+		}
+		printf("\n");
+	}
+}
