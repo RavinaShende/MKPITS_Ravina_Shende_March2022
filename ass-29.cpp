b70c69d5307fc7fbb1a7a306893d571773eef960
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-05-23 10:55:15 +0530

    Read 5 nos & sum of all even values between them.

---
+++ ass-29.cpp
@@ -0,0 +1,27 @@
+// wap that read 5 nos & sum of all even values between them.
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,num[5],sum=0;
+	printf("Enter 1st number :");
+	scanf("%d",&num[0]);
+	printf("\nEnter 2nd number :");
+	scanf("%d",&num[1]);
+	printf("\nEnter 3rd number :");
+	scanf("%d",&num[2]);	
+	printf("\nEnter 4th number :");
+	scanf("%d",&num[3]);
+	printf("\nEnter 6th number :");
+	scanf("%d",&num[4]);
+	for (i=0;i<5;i++)
+	{
+		if(num[i]%2 == 0)
+		{
+			sum= sum+num[i];
+		}
+	}
+	printf("\nSum of all even values=%d",sum);
+	printf("\n");
+}
