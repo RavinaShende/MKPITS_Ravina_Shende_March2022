db486f81ca6e7153e35f4ddd60bbdc967cd23025
RavinaShende <ravina.shende794@gmail.com>
2022-08-12 14:43:27 +0530

    area of rectangle

---
+++ Csharp/Assignment-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass1_area_of_rect._
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept length & breath ,& display the area of rectangle
+            int length, breath, result;
+            Console.WriteLine("Enter Length");
+            length=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Breath");
+            breath=Convert.ToInt32(Console.ReadLine());
+            result = length * breath;
+            Console.WriteLine("Area of rectangle =" + result);
+            Console.ReadLine();
+        }
+    }
+}
