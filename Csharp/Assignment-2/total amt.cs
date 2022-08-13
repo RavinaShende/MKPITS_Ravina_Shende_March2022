ebdc35b0bc5fc930db50d64f48361f48f57afb9d
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 12:39:56 +0530

    accept product details & display total amount

---
+++ Csharp/Assignment-2/total amt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass10_totalamount_
+{
+    internal class Product
+    {
+        static void Main()
+        {
+            //wap to accept product name, product rate , quantity & display total amount
+            string pname;
+            int prate, pqty,total;
+            Console.WriteLine("Enter Product Name:");
+            pname = Console.ReadLine();
+            Console.WriteLine("Enter Product Rate:");
+            prate=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Product Quantity:");
+            pqty=Convert.ToInt32(Console.ReadLine());
+            total = prate * pqty;
+            Console.WriteLine("Total Amount = " + total);
+            Console.ReadLine();
+        }
+    }
+}
