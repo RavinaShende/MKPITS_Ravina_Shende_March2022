739a754c965c99af51a0a257a2748c7bc98cdabf
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 12:13:41 +0530

    accept 2 no & find greater no

---
+++ Csharp/Assignment-2/greater no.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass3_greater_no._
+{
+    internal class greater
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept 2nos & print Greater no.
+            int n1, n2;
+            Console.WriteLine("Enter number 1");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2)
+                Console.WriteLine(n1 + "n1 is greater");
+            else if (n2 > n1)
+                Console.WriteLine(n2 + " is greater");
+            else
+                Console.WriteLine("Both numbers are equal");
+
+            Console.ReadLine();
+        }
+    }
+}
