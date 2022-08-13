36a28c57d1ff6ee8cdbb84cea3b944dc5e25f34a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 12:45:16 +0530

    accept product details & display total amount with discount using if else

---
+++ Csharp/Assignment-2/total amt ifelse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass10_1_totalamount_disc._
+{
+    internal class Product
+    {
+        static void Main()
+        {
+            //wap to accept product name, product rate, quantity & calculate totalamount,
+            //discount ie 50 % if totalamount > 2000,else discount = 10 % of total amount
+            //using if-else
+            string pname;
+            int prate, pqty, total;
+            Console.WriteLine("Enter Product Name:");
+            pname = Console.ReadLine();
+            Console.WriteLine("Enter Product Rate:");
+            prate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Product Quantity:");
+            pqty = Convert.ToInt32(Console.ReadLine());
+            total = prate * pqty;
+            Console.WriteLine("Total Amount = " + total);
+            float disc = 0;
+            if (total > 2000)
+                disc = (total * 50)/100;
+            else
+                disc = (total * 10) / 100;
+
+
+            float tamt = total - disc;
+            Console.WriteLine("Payable amount = " + tamt);
+            Console.ReadLine();
+        }
+    }
+}
