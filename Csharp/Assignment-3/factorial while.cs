be09c96a9677403079ab557f5da5fd906c16997a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 13:38:27 +0530

    accept a number and print factorial of a number using while loop

---
+++ Csharp/Assignment-3/factorial while.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass13_Fact_while_
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept no & calculate factorial
+            int num, i=1;
+            int fact = 1;
+            Console.WriteLine("Enter Number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            while(i <= num)
+            {
+                fact = fact * i;
+                i++;
+            }
+            Console.WriteLine("Factorial = " + fact);
+            Console.ReadLine();
+        }
+    }
+}
