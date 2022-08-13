dbcb7a5706ee522d691001e2ab7d9a3bfb585663
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 19:17:48 +0530

    create a class car having fields make, model and method getcardata and displaycardata

---
+++ Csharp/Assignment-5/class for car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//create a class car having fields make,model
+//and method getcardata and displaycardata
+namespace ass23_create_class_for_car_
+{
+    class Cardata
+    {
+        int carno;
+        string carmake;
+        string carmodel;
+        float carprice;
+        
+        public void getcardata()
+        {
+            Console.WriteLine("Enter car no. :");
+            carno=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter car make :");
+            carmake=Console.ReadLine();
+            Console.WriteLine("Enter car model :");
+            carmodel=Console.ReadLine();
+            Console.WriteLine("Enter car price :");
+            carprice=Convert.ToInt32(Console.ReadLine());
+        }
+        public void displaycardata()
+        {
+            Console.WriteLine("Car no. :"+carno);
+            Console.WriteLine("Car make :" + carmake);
+            Console.WriteLine("Car model no. :" + carmodel);
+            Console.WriteLine("Car Price :" + carprice);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cardata c = new Cardata();
+            c.getcardata();
+            c.displaycardata();
+            Console.ReadLine();
+        }
+    }
+}
