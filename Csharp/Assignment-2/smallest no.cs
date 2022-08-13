b2ad597ab648e888b5ae171db9f1354439c8e4e1
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 12:19:07 +0530

    accept 3 no & find smallest no.

---
+++ Csharp/Assignment-2/smallest no.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass5_smallest_no_
+{
+    internal class smallest
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept 3 nos & print smallest no.
+            int n1, n2, n3;
+            Console.WriteLine("Enter 3 different numbers :");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 < n2 && n1 < n3)
+                Console.WriteLine(n1 + " is smallest number");
+            else if (n2 < n1 && n2 < n3)
+                Console.WriteLine(n2 + " is smallest number");
+            else  (n3 < n1 && n3 < n2)
+                Console.WriteLine(n3 + " is smallest number");
+
+            Console.ReadLine();
+        }
+    }
+}
