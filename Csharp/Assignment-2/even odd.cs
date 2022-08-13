4fea2b9a710a6a54200bd5bf9c2f6a7702fae7d5
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 12:28:54 +0530

    accept a no. and print whether it is even no or odd no.

---
+++ Csharp/Assignment-2/even odd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass7_even_odd_
+{
+    internal class evenodd
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept a no. & print wheather it is even or odd no.
+            int num;
+            Console.WriteLine("Enter any number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.WriteLine(num + " is even number");
+            else
+                Console.WriteLine(num + " is odd number");
+
+            Console.ReadLine();
+        }
+    }
+}
