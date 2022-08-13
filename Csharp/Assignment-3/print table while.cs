938822f20cb6ebd2615756215938f58f4d405e12
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 13:21:51 +0530

    accept a number & print table using while loop

---
+++ Csharp/Assignment-3/print table while.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass12_printtable_while_
+{
+    internal class Table
+    {
+        static void Main()
+        {
+            //wap to accept number & print a table using while loop
+            int num;
+            int res = 0;
+            Console.WriteLine("Enter number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while( i <= 10)
+            {
+                res = num * i;
+                Console.WriteLine("{0} * {1} = {2}", num, i, res);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
