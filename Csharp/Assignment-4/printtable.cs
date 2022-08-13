74faa99b1b38a7eb040a3459ce637262a0f1f737
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 14:52:09 +0530

    create a method to accept a number and print the table of that number

---
+++ Csharp/Assignment-4/printtable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass16_print_table_
+{
+    //Create a method to
+    //accept a number and print the table of that number.
+    internal class Program
+    {
+        static void PrintTable(int n)
+        {
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine("{0}*{1}={2}",n,i,n*i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter number :");
+            num=Convert.ToInt32(Console.ReadLine());
+            PrintTable(num);
+            Console.ReadLine();
+        }
+    }
+}
