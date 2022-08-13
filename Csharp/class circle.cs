1406a061eeb0bcc95d9746a00fcdc6c66aa67fce
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-13 19:20:18 +0530

    create a class cirlce having fields radius and method calculatearea and calculatecirc

---
+++ Csharp/class circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//create a class cirlce having fields radius
+//and method calculatearea and calculatecirc
+namespace ass25_create_class_circle__
+{
+    class Circle
+    {
+        public int rad;
+        public float area = 0;
+        public float circ = 0;
+
+        public void areadata()
+        {
+            area = 3.14f * rad * rad;
+            Console.WriteLine("Area of circle = " + area);
+        }
+        public void circdata()
+        {
+            circ = 2 * 3.14f * rad;
+            Console.WriteLine("Circ of circle = " + circ);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            Console.WriteLine("Enter Radius of circle :");
+            c1.rad=Convert.ToInt32(Console.ReadLine());
+            c1.areadata();
+            c1.circdata();
+            Console.ReadLine();
+        }
+    }
+}
