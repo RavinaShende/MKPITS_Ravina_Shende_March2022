3746adb59e18fd2e38eb9ddebf1a6a471e9b2a70
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 13:25:41 +0530

    wap to accept empname and designation and using switch display bonus

---
+++ Csharp/Practice Program/clerk-manager switch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg2_5
+{
+    internal class Office
+    {
+        static void Main(string[] args)
+        {
+            string empname, designation;
+            int bonus = 0;
+            Console.WriteLine("Enter Employe Name & Designation");
+            empname = Console.ReadLine();
+            designation = Console.ReadLine();
+
+            switch (designation)
+            {
+                case "Manager":
+                    bonus = 10000;
+                    break;
+                case "clerk":
+                    bonus = 5000;
+                    break;
+                case "peon":
+                    bonus = 2000;
+                    break;
+                default:
+                    Console.WriteLine("Invalid designation");
+                    break;
+            }
+            Console.WriteLine("Employe Name = {0} , Bonus = {1}", empname, bonus);
+            Console.ReadLine();
+        }
+    }
+}
