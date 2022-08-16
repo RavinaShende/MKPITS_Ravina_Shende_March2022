7bff88049614d35174b16a26f434c426ea99113f
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 13:02:30 +0530

    short datatype

---
+++ Csharp/Practice Program/Program10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short b1;
+            Console.WriteLine("Enter short number");
+            b1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("short entered = " + b1);
+            Console.ReadLine();
+        }
+    }
+}
