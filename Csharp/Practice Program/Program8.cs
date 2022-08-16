fe6b6d5c2b794b1145a35d7720ddc6702000d99c
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 12:59:24 +0530

    convert byte program(0-255)

---
+++ Csharp/Practice Program/Program8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program8
+{
+    internal class Byte
+    {
+        static void Main(string[] args)
+        {
+            //Program on byte
+            byte b1;
+            Console.WriteLine("Enter Byte");
+            b1 = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Byte Entered = " + b1);
+            Console.ReadLine();
+        }
+    }
+}
