5e90ecb7cd82cd6b1d9f73f27d4829d5859f2002
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 13:59:33 +0530

    program on goto statement

---
+++ Csharp/Practice Program/Prg3-9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg3_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // program on goto statement
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                goto even;
+            Console.WriteLine(num + " is odd number");
+            goto end;
+        even:
+            Console.WriteLine(num + " is even number. ");
+            goto end;
+        end:
+            Console.WriteLine("bye");
+            Console.ReadLine(); 
+        }
+    }
+}
