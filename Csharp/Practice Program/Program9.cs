bf4bcf04da3866d4e7f034c2b5a9a1e0c7bfc31a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 13:00:30 +0530

    Convert to

---
+++ Csharp/Practice Program/Program9.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program9
+{
+    internal class bite
+    {
+        static void Main(string[] args)
+        {
+            sbyte b1;
+            Console.WriteLine("Enter byte");
+            b1 = Convert.ToSByte(Console.ReadLine());
+            Console.WriteLine("Entered sbyte = " + b1);
+            Console.ReadLine();
+        }
+    }
+}
