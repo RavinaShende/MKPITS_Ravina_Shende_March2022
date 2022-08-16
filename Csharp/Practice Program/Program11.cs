6120d68fc01a804ddb54da0b07f5e04062a7e8b7
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 13:05:03 +0530

    ushort datatype  (unsigned short to accept only positive no.)

---
+++ Csharp/Practice Program/Program11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort b1;
+            Console.WriteLine("Enter ushort number");
+            b1 = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine("ushort entered = " + b1);
+            Console.ReadLine();
+        }
+    }
+}
