c3c98556d146751971f9ad1f3ce04b2b473e169e
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 13:56:10 +0530

    create an array of 3 elements using foreach loop

---
+++ Csharp/Practice Program/Prg3-7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg3_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an array of 3 elements using foreach loop
+            string[] num = new string[3];
+            num[0] = "amit";
+            num[1] = "raj";
+            num[2] = "dilip";
+
+            foreach(string i in num)
+            {
+                Console.WriteLine(i);   
+            }
+            Console.ReadLine();
+        }
+    }
+}
