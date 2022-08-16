92b4c60e28431889d07542d9efa7ecedd93d971e
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 14:03:22 +0530

    Method with parameter & return type

---
+++ Csharp/Practice Program/Prg3-14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg3_14_w_a_w_r_
+{
+    internal class Program
+    {
+        //Method with parameter & return type
+        static int square(int num)
+        {
+            int sq = num * num;
+            return sq;
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            int res = square(num);
+            Console.WriteLine("Square = " + res);
+            Console.ReadLine();
+        }
+    }
+}
