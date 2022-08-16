e977203fc5d08f5cbaf6212ef6c046a8034ad439
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 12:55:10 +0530

    wap to accept radius of a circle and display the area and circumferance of circle.

---
+++ Csharp/Practice Program/Program5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept radius of circle & display area & circumference
+            int radius;
+            Console.WriteLine("Enter radius of circle");
+            radius = Convert.ToInt32(Console.ReadLine());
+            float area = 3.14f * radius * radius;
+            Console.WriteLine("Area of Circle = " + area);
+            float circ = 2 * 3.14f * radius;
+            Console.WriteLine("Circumference of circle =" + circ);
+            Console.ReadLine(); 
+        }
+    }
+}
