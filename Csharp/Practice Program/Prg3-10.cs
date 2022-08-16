67f37124cac575124f761abee82a526cf831139f
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 14:00:18 +0530

    wap to create a display function w/o parameter

---
+++ Csharp/Practice Program/Prg3-10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg3_10_user_define_
+{
+    internal class User_define_func
+    {
+        static void display()
+        {
+            Console.WriteLine("Hello from display function");
+        }
+        static void Main(string[] args)
+        {
+            //wap to create a display function w/o parameter
+            display();
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
