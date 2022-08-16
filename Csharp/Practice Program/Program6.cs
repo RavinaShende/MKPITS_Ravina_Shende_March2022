1de1e98c7dc6145c4bb9ecaa4207be04c1a7b7c9
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 12:56:28 +0530

    write a program to accept character from the user

---
+++ Csharp/Practice Program/Program6.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept character from the user
+            char ch;
+            Console.WriteLine("Enter character");
+            ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Charater entered = " + ch);
+            Console.ReadLine();
+        }
+    }
+}
