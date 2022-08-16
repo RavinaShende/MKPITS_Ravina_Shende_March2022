0e80c2ed38c2ff19663827230de6a552699667d8
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 12:27:18 +0530

    accept a number and print the square of that number

---
+++ Csharp/Practice Program/Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept a number & print the square of that number
+            int num;
+            Console.WriteLine("Enter any number");
+            num = Convert.ToInt32(Console.ReadLine());//ToInt32 method will convert string to integer
+            int square = num * num;
+            Console.WriteLine("square =" + square);
+            Console.ReadLine();
+
+        }
+    }
+}
