72b9e7e855d20cf432c1cdd4d1b3aecc1156406f
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 12:53:19 +0530

    wap to accept 2 no. and print addition of 2 no.

---
+++ Csharp/Practice Program/Program4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept 2 no & print the addition of numbers
+            int num1, num2, res;
+            Console.WriteLine("enter number 1");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 2");
+            num2=Convert.ToInt32(Console.ReadLine());
+            res = num1 + num2;
+            Console.WriteLine("addition =" + res);
+            Console.ReadLine();
+        }
+    }
+}
