ff8f5809bfaa9f6121d3fd14b4014ba9eea63c9f
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 13:18:13 +0530

    accept a age from the user and print whether eligible to vote or not

---
+++ Csharp/Practice Program/Prg2-1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg2_1
+{
+    internal class check
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter age");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+                Console.WriteLine("Not Eligible for Vote");
+            else
+                Console.WriteLine("Eligible for Vote");
+            Console.ReadLine();
+        }
+    }
+}
