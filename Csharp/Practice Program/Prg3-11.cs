ce2ef8967511986dde65e321c13224e302d9ffb5
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-16 14:01:01 +0530

    wap to create a function to square a number

---
+++ Csharp/Practice Program/Prg3-11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg3_11_square_
+{
+    internal class Number
+        // wap to create a function to square a number
+    {
+        static void square()
+        {
+            int num = 5;
+            int sq = num * num;
+            Console.WriteLine("square = " + sq);
+        }
+        static void Main(string[] args)
+        {
+            square();
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+       
+    }
+}
