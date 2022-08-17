ac604a81d741bcf02aa27ca408e1df754a29101d
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-18 01:29:08 +0530

    Program on protected class

---
+++ Csharp/Practice Program/Prg10-6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg10_6
+{
+    class ProtectedTest
+    {
+        protected string name = "shrikant";
+        protected void Msg(string msg)
+        {
+            Console.WriteLine("Hi "+msg);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProtectedTest p = new ProtectedTest();
+            //accessing protected variable
+            Console.WriteLine("Hello " + p.name);//error
+            //accessing protected function
+            protectedTest.Msg("Swami Ayyer");//error
+           Console.ReadLine();
+        }
+    }
+}
