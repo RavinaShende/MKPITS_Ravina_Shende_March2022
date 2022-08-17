61e6036147bf58b5698a0c278a67130ee1c21e29
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-17 14:16:28 +0530

    Program on OUT method

---
+++ Csharp/Practice Program/Prg4-4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg4_4_example_of_out_
+{
+    internal class Program
+    {
+        static void display(out int num)
+        {
+            num = 5;
+            Console.WriteLine("num inside method " + num);//5
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            display(out num);
+            Console.WriteLine("num inside main " + num);//5
+            Console.ReadLine();
+        }
+    }
+}
