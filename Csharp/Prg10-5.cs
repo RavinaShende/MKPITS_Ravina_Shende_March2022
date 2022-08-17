218429f073937a176ca61f96c096f920b090e798
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-18 01:27:01 +0530

    creating a child class student inherited from person class

---
+++ Csharp/Prg10-5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg10_5
+{
+    class Person
+    {
+        protected string name;
+    }
+    //creating a child class student inherited from person class
+    class Student : Person
+    {
+        public void getdata()
+        {
+            name = "Ravi";
+            Console.WriteLine("name "+name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.getdata();
+            Console.ReadLine();
+        }
+    }
+}
