7069e89d8ab7d990097d2f07d7b13fd8ff8786f3
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-18 01:30:15 +0530

    Program on protected test

---
+++ Csharp/Practice Program/Prg10-7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg10_7
+{
+    class ProtectedTest
+    {
+        protected string name = "shrikant";
+        protected void Msg(string msg)
+        {
+            Console.WriteLine("Hi " + msg);
+        }
+    }
+    internal class Program : ProtectedTest
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //accessing protected variable
+            Console.WriteLine("Hello " + p.name);//error corrected
+            //accessing protected function
+            p.Msg("Swami Ayyer");//error corrected
+            Console.ReadLine();
+        }
+    }
+}
