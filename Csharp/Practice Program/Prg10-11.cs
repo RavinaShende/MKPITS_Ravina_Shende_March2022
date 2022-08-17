0203037e17a3d7a6a88174135ad93b04c87937f6
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-18 01:34:05 +0530

    Program on Private Access Specifier

---
+++ Csharp/Practice Program/Prg10-11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg10_11_private_access_specifier_
+{
+    class PrivateTest
+    {
+        private string name = "Santosh Kumar";
+        private void Msg(string msg)
+        {
+            Console.WriteLine("Hi "+msg);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateTest test = new PrivateTest();
+            //accessing private variable
+            Console.WriteLine("Hello " + test.name);
+            //accessing private function
+            test.Msg("Peter Decosta");
+            Console.ReadLine();
+        }
+    }
+}
