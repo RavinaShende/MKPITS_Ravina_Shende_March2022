a8fcbb403e625405fcdb531fc16357c8425e37f1
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-17 14:31:30 +0530

    Program on clear method

---
+++ Csharp/Practice Program/Prg4-14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg4_14_clear_method_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 8, 5, 4, 7, 6, 3, 1, 2, 3 };
+            Array.Clear(num, 0, 2);
+            //display array elements
+            for(int i=0;i<num.Length;i++)
+            {
+                Console.Write(num[i] + "\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
