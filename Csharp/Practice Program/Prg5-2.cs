3c9be7fc58af719d130876df7c394431dd144254
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-17 15:13:14 +0530

    wap to accept data from user & create a class employee having fields empno, empname & salary

---
+++ Csharp/Practice Program/Prg5-2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg5_2_create_class_
+{
+    //create a class employee having fields empno, empname & salary
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float salary;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an object (instance) of employee class
+            // syntax : classname objectname = new classname();
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter Employee No");
+            emp.empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Emter Employee Name");
+            emp.empname = Console.ReadLine();
+            Console.WriteLine("Enter Employee salary");
+            emp.salary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("empno" + emp.empno);
+            Console.WriteLine("empname " + emp.empname);
+            Console.WriteLine("empsalary"+emp.salary);
+            Console.ReadLine();
+
+        }
+    }
+}
