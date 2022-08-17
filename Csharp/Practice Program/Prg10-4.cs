7c9a52ca75b91eb3daa5ebcb5e84dbaa59ec4ebd
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-18 01:25:51 +0530

    Program on protected access specifier

---
+++ Csharp/Practice Program/Prg10-4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg10_4_protected_access_specifier_
+{
+    class Student
+    {
+        protected string name;
+        public void getdata()
+        {
+            name = "Ravi";
+            Console.WriteLine("Name "+name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.getdata();
+            Console.ReadLine();
+        }
+    }
+}
