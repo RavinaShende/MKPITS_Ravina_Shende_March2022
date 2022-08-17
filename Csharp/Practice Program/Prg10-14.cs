cb6e40983d7da3a697bf9580a19801ce4f107e7a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-18 01:40:54 +0530

    Program

---
+++ Csharp/Practice Program/Prg10-14.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg10_14
+{
+    class Student
+    {
+        int rno;
+        string name;
+
+        public Student()
+        {
+            rno = 11;
+            name = "amitabh";
+        }
+        public Student(int rno, string name)
+        {
+            this.rno = rno;
+            this.name = name;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("rno " + rno);
+            Console.WriteLine("name " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.displaydata();
+            Student s1 = new Student(111,"rajesh");
+            s1.displaydata();
+            Console.ReadLine();
+        }
+    }
+}
