218caef477075269c475eab0f434ca826f1d4ab1
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-17 14:26:54 +0530

    create a num array of 2 rows & 3 columns

---
+++ Csharp/Practice Program/Prg4-11.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg4_11_2D_array_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a num array of 2 rows & 3 columns
+            int[,] num = new int[2, 3];
+            for(int i=0;i<2;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    Console.WriteLine("Enter num");
+                    num[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            //display array elements
+            for(int i=0;i<2;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    Console.Write(num[i, j] + "\t");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
