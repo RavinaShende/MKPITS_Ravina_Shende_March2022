cc0981226a641b8304358e04a852d25027fa350d
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-17 14:14:04 +0530

    Program on pass by value method

---
+++ Csharp/Practice Program/Prg4-2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg4_2_passbyvalue_
+{
+    internal class Program
+    {
+        static void display(int num)
+        {
+            num++;
+            Console.WriteLine("Num inside method " + num);
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            display(num);
+            Console.WriteLine("Num inside main " +num);
+            Console.ReadLine();
+        }
+    }
+}
