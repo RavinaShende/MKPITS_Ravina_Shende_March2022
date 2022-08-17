991ea171318dbcf972f7f3fdf5de87afc03c3366
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-17 14:20:26 +0530

    accept array parameters & sum of all array parameters & display it

---
+++ Csharp/Practice Program/Prg4-6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg4_6
+{
+    internal class Program
+    {
+        static void display(params int[] num)
+        {
+            int sum = 0;
+            for(int i=0;i<num.Length;i++)
+            {
+                sum = sum + num[i];
+            }
+            Console.WriteLine(sum);
+        }
+        static void Main(string[] args)
+        {
+            display(2, 3, 4);
+            Console.ReadLine();
+        }
+    }
+}
