0e2222c3cb3c86da3dd64894ae1ce7c2973f91ed
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-01 00:01:16 +0530

    create a base class for person & derived class for student by using protected access modifier

---
+++ Csharp/Practice Program/Prg11-7.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg11_7_protected_access_modifier_
+{
+    public class Person
+    {
+        string name;
+        string address;
+        protected void getPersonData(string name,string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        protected void displayPersonData()
+        {
+            
+            Console.WriteLine("name  " + name);
+            Console.WriteLine("address "+address);
+        }
+    }
+    public class Student : Person
+    {
+        int rno;
+        int marks;
+        public void getStudentData(int rno, int marks)
+        {
+            getPersonData("Africa", "Nagpur");//call base class method from derived class
+            this.rno = rno;
+            this.marks = marks;
+        }
+        public void displayStudentData()
+        {
+            displayPersonData(); //calling base class method from derived class
+            Console.WriteLine("rno :" + rno);
+            Console.WriteLine("marks :" + marks);
+        }
+    }
+    class Testinheritance
+    {
+        public static void Main(string[] args)
+        {
+            Student p1 = new Student();
+            p1.getStudentData(111, 999);
+            p1.displayStudentData();
+            //p1.getPersonData("arifa", "nagpur"); //since testinhertance class in not derived class of person
+            //p1.displayPersonData(); //since testinhertance class in not derived class of person
+            Console.ReadLine();
+        }
+    }
+}
