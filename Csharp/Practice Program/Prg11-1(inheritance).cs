bfd842cae086f0877f7df33245087cebd03c8b29
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-31 23:40:54 +0530

    create a base class person & derieved class student

---
+++ Csharp/Practice Program/Prg11-1(inheritance).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg11_1_inheritance_
+{
+    //creating a base class
+    class Person
+    {
+       public string name;
+    }
+    //creating a derived class student
+    class Student : Person
+    {
+        public int rno;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.rno = 111;
+            s.name = "Rajesh";
+            Console.WriteLine("Roll no :" + s.rno);
+            Console.WriteLine("Name : " + s.name);
+            Console.ReadLine();
+        }
+    }
+}
