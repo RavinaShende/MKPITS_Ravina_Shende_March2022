d9cb0f51b9c9c790b0b6f60ebdf5805cf8a71473
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-08-31 23:53:02 +0530

    create a base class animal & create derived class dog

---
+++ Csharp/Practice Program/Prg11-5(Single Level Inheritance ).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg11_5
+{
+    //create a base class
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating..");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void bark()
+        {
+            Console.WriteLine("Barking..");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.eat();
+            d1.bark();
+            Console.ReadLine();
+        }
+    }
+}
