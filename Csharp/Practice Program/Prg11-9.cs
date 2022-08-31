fb86394f8e1f3a657ec0c9ee071df42043d6fa95
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-01 00:08:18 +0530

    create a base class for person & derived class for employee using protected internal access modifier

---
+++ Csharp/Practice Program/Prg11-9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg11_9_protected_internal_access_modifier_
+{
+    class Person
+    {
+        string name;
+        protected void  getPersonData(string name)
+        {
+            this.name = name;
+            Console.WriteLine("name = " + name);
+        }
+    }
+    class Employee : Person
+    {
+        int empno;
+        internal void getEmployeeData(int empno, string name)
+        {
+            this.empno = empno;
+            Console.WriteLine("empno = " + empno);
+            getPersonData(name); // calling base class
+        }
+            
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.getEmployeeData(11, "shreyash");
+            Console.ReadLine();
+        }
+    }
+}
