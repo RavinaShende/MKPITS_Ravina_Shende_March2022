76ac3015198617410aad12f5d151270aef78d112
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-01 00:14:57 +0530

    create a base class for animal & derived class for dog & babydog, using multilevel inheritance

---
+++ Csharp/Practice Program/Prg11-11.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg11_11
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+    public class Dog:Animal
+    {
+        public void bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+    public class BabyDog:Dog
+    {
+        public void weep()
+        {
+            Console.WriteLine("Weeping...");
+        }
+    }
+    internal class TestInheritance
+    {
+        public static void Main(string[] args)
+        {
+            BabyDog d1 = new BabyDog();
+            d1.eat();
+            d1.bark();
+            d1.weep();
+            Console.ReadLine();
+        }
+    }
+}
