34c3d15d1f27af064219c740eb7c6a63e06c3c0c
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:52:51 +0530

    example of method overriding

---
+++ Csharp/Practice Program/Prg17-9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg17_9_method_overrideing_
+{
+    public class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating....");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating bread...");
+        }
+    }
+    public class TestOverride
+    {
+        public static void Main(string[] args)
+        {
+            Animal d = new Dog();
+            d.eat();
+            Console.ReadLine();
+        }
+    }
+}
