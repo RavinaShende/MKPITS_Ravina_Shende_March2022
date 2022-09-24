48a49a5461e64cb363b6b8fbd47be7fe1924d1f5
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:10:53 +0530

    Example of runtime polymorphism

---
+++ Csharp/Practice Program/Prg16-8.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg16_8
+{
+    public class Shape
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("drawing..");
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drawing rectangle..");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drawing circle..");
+        }
+    }
+    internal class TestPolymorphism
+    {
+        public static void Main(string[] args)
+        {
+            Shape s;
+            s = new Shape();
+            s.draw();
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+            Console.ReadLine();
+        }
+    }
+}
