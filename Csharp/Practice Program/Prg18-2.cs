da6517de1228d2e09d9f1305db988b854361f974
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 02:11:53 +0530

    Example of namespace where one namespace program accesses another namespace program.

---
+++ Csharp/Practice Program/Prg18-2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg18_2
+{
+    namespace first
+    {
+        public class Hello
+        {
+            public void sayHello()
+            {
+                Console.WriteLine("Hello From First Namespace");
+            }
+        }
+    }
+    namespace second
+    {
+        public class Hello
+        {
+            public void sayHello()
+            {
+                Console.WriteLine("Hello from second namespace");
+            }
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            first.Hello h1 = new first.Hello();
+            second.Hello h2 = new second.Hello();
+            h1.sayHello();
+            h2.sayHello();
+            Console.ReadLine();
+        }
+    }
+}
