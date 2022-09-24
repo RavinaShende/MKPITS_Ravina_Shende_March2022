ce35714be63eaae966ea497b99cef120761587f3
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 02:31:50 +0530

    Example of static method

---
+++ Csharp/Practice Program/Prg18-10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg18_10
+{
+    class Student
+    {
+        public static void display()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void sayhi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+    internal class Program
+    {
+        //creating nonstatic display method
+        static void Main(string[] args)
+        {
+            Student.display();
+            //Student.sayhi();//error
+            Student s = new Student();
+            s.sayhi();
+            Console.ReadLine();
+        }
+    }
+}
