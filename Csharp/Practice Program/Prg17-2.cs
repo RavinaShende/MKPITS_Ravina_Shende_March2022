3e70d3a36280d29925b2bc09b4df77f958166dc5
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:43:08 +0530

    example on abstract and non-abstract methods.

---
+++ Csharp/Practice Program/Prg17-2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg17_2
+{
+    abstract class Account
+    {
+        public abstract void deposit(); // abstract method
+        //non abstract method
+        public void showbalance()
+        {
+            Console.WriteLine("Balance method");
+        }
+    }
+    class Saving : Account
+    {
+        public override void deposit()
+        {
+            Console.WriteLine("Hello from saving deposit method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //account act=new account(); error
+            Account act = new Saving();
+            act.deposit();
+            act.showbalance();
+            Console.ReadLine();
+        }
+    }
+}
