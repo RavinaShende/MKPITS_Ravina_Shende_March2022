6ba4465ecee8284cb5b9b5111450e01209b879c7
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:55:53 +0530

    example of interface in which has draw() method & its implementation is provided by two classes: Rectangle and Circle.

---
+++ Csharp/Practice Program/Prg17-10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg17_10_interface_
+{
+    interface Shape
+    {
+        void draw();
+    }
+    class Rectangle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("draw method of rectangle class");
+        }
+    }
+    class Circle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("draw method of circle class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s;
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+            Console.ReadLine();
+        }
+    }
+}
