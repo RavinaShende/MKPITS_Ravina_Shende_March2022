3df999b09be13de1a946bf894f9d8c8e6067f16a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:04:44 +0530

    Program on accesses the field of base class

---
+++ Csharp/Practice Program/Prg16-2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg16_2
+{
+    class Person
+    {
+        public string name = "MKPITS";
+    }
+    class Student:Person
+    {
+        public string name = "Roshan";
+        public void display()
+        {
+            Console.WriteLine("name = " + name); //Roshan
+            Console.WriteLine("base class field name = " + base.name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.display();
+            Console.ReadLine();
+        }
+    }
+}
