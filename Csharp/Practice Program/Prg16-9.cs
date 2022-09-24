ffbe6f6deb288903f477e254132667ab6e692540
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:12:20 +0530

    Program on runtime polymorphism with data members

---
+++ Csharp/Practice Program/Prg16-9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg16_9
+{
+    public class Animal
+    {
+        public string color = "white";
+    }
+    public class Dog:Animal
+    {
+        public string color = "black";
+    }
+
+    internal class TestSealed
+    {
+        public static void Main(string[] args)
+        {
+            Animal d = new Dog();
+            Console.WriteLine(d.color);
+            Dog d1 = new Dog();
+            Console.WriteLine(d1.color);
+            Console.ReadLine();
+        }
+    }
+}
