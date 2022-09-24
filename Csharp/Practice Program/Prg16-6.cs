49298dd262f8d3097e548d5dd06478a3776bf28b
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:08:20 +0530

    Program on calling base constructor

---
+++ Csharp/Practice Program/Prg16-6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg16_6
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("animal..");
+        }
+    }
+    public class Dog:Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("dog..");
+        }
+    }
+
+    internal class TestOverriding
+    {
+        public static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            Console.ReadLine();
+        }
+    }
+}
