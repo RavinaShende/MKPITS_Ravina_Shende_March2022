11b59a87b591154a45ad5b773f0e8314ebfac300
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:06:11 +0530

    Program on calling base class method

---
+++ Csharp/Practice Program/Prg16-4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg16_4
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void eat()
+        {
+            base.eat(); //this will class base class method eat
+            Console.WriteLine("Eating bread...");
+        }
+    }
+    internal class TestBase
+    {
+        public static void Main(string[] args)
+        {
+            Dog g = new Dog();
+            g.eat();
+            Console.ReadLine();
+        }
+    }
+}
