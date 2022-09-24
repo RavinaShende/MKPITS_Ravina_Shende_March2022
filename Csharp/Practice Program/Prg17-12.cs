e585a5f72339d0197e871cc8755c711eae49c044
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:58:23 +0530

    example on interface method

---
+++ Csharp/Practice Program/Prg17-12.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg17_12
+{
+    interface bank
+    {
+        string deposit(int actno, int amt);
+    }
+    class Saving : bank
+    {
+        int actno;
+        int bal = 1000;
+        public string deposit (int actno, int amt)
+        {
+            this.actno = actno;
+            bal = bal + amt;
+            return "deposited successfully, bal is " + bal;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bank b;
+            b = new Saving();
+            String str = b.deposit(1, 200);
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
