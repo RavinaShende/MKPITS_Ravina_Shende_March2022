9c8fb5e021ec57f7321a3d8160fccf9bb12b3e0a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:03:59 +0530

    Program on accesses the field of base class

---
+++ Csharp/Practice Program/Prg16-1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg16_1_Access_base_class_field_
+{
+    public class Animal
+    {
+        public string color = "white";
+    }
+    public class Dog : Animal
+    {
+        string color = "black";
+        public void showColor()
+        {
+            Console.WriteLine(base.color);
+            Console.WriteLine(color);
+        }
+    }
+    internal class TestBase
+    {
+        public static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.showColor();
+            Console.ReadLine();
+        }
+    }
+}
