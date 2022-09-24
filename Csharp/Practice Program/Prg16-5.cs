b38063683c5a6decbdf98313643289c5de83c19a
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:07:17 +0530

    Program on calling base constructor

---
+++ Csharp/Practice Program/Prg16-5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//calling base class constructor internally
+namespace Prg16_5
+{
+    class Person
+    {
+        string name = "MKPITS";
+        public Person()
+        {
+            Console.WriteLine("person class constructor called");
+        }
+        public void display()
+        {
+            Console.WriteLine("base class field name = " + name);
+        }
+    }
+    class Student : Person
+    {
+        string name = "Roshan";
+        public Student()
+        {
+            Console.WriteLine("student class constructor called");
+        }
+        public void display()
+        {
+            //calling base class display method
+            base.display();
+            Console.WriteLine("child class field name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.display();
+            Console.ReadLine();
+        }
+    }
+}
