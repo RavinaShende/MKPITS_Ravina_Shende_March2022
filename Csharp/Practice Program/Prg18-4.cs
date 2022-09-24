6e2a4d9d341c92b28e5f841d0b4000cb62ebec23
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 02:14:18 +0530

    Example on nested namespace

---
+++ Csharp/Practice Program/Prg18-4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Prg18_4_nested_namespace_
+{
+    using First;
+    using First.Second;
+    namespace First
+    {
+        public class Hello
+        {
+            public void sayHello()
+            {
+                Console.WriteLine("Hello first namespace");
+            }
+        }
+        //creating a nested namespace
+        namespace Second
+        {
+            public class Welcome
+            {
+                public void sayWelcome()
+                {
+                    Console.WriteLine("Hello second namespace");
+                }
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hello h1 = new Hello();
+            h1.sayHello();
+            Welcome w = new Welcome();
+            w.sayWelcome();
+            Console.ReadLine();
+        }
+    }
+}
