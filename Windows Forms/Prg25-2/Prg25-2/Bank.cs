99d0ea8712b81e1ccecb4ae96dd03fcd0b856b30
RavinaShende <ravina.shende794@gmail.com>
2022-09-25 04:41:50 +0530

    windows form app. for bank account query

---
+++ Windows Forms/Prg25-2/Prg25-2/Bank.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg25_2
+{
+    internal class Bank
+    {
+        public int accountno;
+        public int balance = 1000;
+        public string deposit(int amount)
+        {
+            balance = balance + amount;
+            return "Amount deposited successfully, balence is " + balance.ToString();
+        }
+        public string withdrawl(int amount)
+        {
+            string res = null;
+            if(amount > balance )
+            {
+                res = "insufficient fund, cannot withdraw money";
+            }
+            else
+            {
+                balance = balance - amount;
+                res = "amount withdrawl successfully, balance is" + balance.ToString();
+            }
+            return res;
+        }
+        public string showbalance()
+        {
+            return "balance amount is " + balance.ToString();
+        }
+    }
+}
---
+++ Windows Forms/Prg25-2/Prg25-2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prg25_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bank b = new Bank();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b.accountno = Convert.ToInt32(textBox1.Text);
+            string str = b.deposit(Convert.ToInt32(textBox2.Text));
+            label3.Text = str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b.accountno = Convert.ToInt32(textBox1.Text);
+            string str = b.withdrawl(Convert.ToInt32(textBox2.Text));
+            label3.Text = str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            b.accountno= Convert.ToInt32(textBox1.Text);
+            String str = b.showbalance();
+            label3.Text = "account no:" + b.accountno + " : " + str;
+        }   
+    }
+}
