363286087d866f7c95ac5990ba2821b0bdec06f8
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:05:38 +0530

    Program on calling base class method

---
+++ Csharp/Practice Program/Prg16-3(calling base class method).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg16_3_calling_base_class_method_
+{
+    class Person
+    {
+        string name = "MKPITS";
+        public void display()
+        {
+            Console.WriteLine("base class field name = " + name);
+        }
+    }
+    class Student:Person
+    {
+        string name = "Roshan";
+        public void display()
+        {
+            //calling base class display method
+            base.display();
+            Console.WriteLine("child class field name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.display();
+            Console.ReadLine();
+        }
+    }
+}
