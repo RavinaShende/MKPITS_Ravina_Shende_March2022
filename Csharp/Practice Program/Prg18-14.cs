875da20a7a6438fb703c5e08535ddf5cc2c8a2a4
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 02:37:17 +0530

    Example of static class that contains static field and static method

---
+++ Csharp/Practice Program/Prg18-14.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg18_14
+{
+    public static class MyMath
+    {
+        public static float PI = 3.14f;
+        public static int cube(int n)
+        {
+            return n * n * n;
+        } 
+    }
+    public class TestMyMath
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Value of PI is : " + MyMath.PI);
+            Console.WriteLine("Cube of 3 is : " + MyMath.cube(3));
+            Console.ReadLine();
+        }
+    }
+}
