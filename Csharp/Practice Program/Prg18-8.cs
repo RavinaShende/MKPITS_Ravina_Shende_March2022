52c792d677131ab2b64ab6627ee6a1e7e648c8c4
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 02:29:17 +0530

    Example on static keyword

---
+++ Csharp/Practice Program/Prg18-8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg18_8_Static_
+{
+    internal class Program
+    {
+        void display()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.display();
+            Console.ReadLine();
+        }
+    }
+}
