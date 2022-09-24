05c9fd1ddea20a04357f4b1a5c4e45b4b59221be
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:41:39 +0530

    Example on abstract class method

---
+++ Csharp/Practice Program/Prg17-1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg17_1_abstract_class_
+{
+    abstract class Account
+    {
+        public abstract void deposit();
+    }
+    class Saving : Account
+    {
+        public override void deposit()
+        {
+            Console.WriteLine("Hello from saving deposit method");
+        }
+    }
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+             //Account act = new Account(); error
+            Account act = new Saving();
+            act.deposit();
+            Console.ReadLine();
+        }
+    }
+}
