b64e4018982f437efd74c9f881f8a6c9a5f917a2
RavinaShende <ravina.shende794@gmail.com>
2022-09-25 03:56:12 +0530

    Example on addition of 2nos form

---
+++ Windows Forms/Prg22-2(addition of 2no)/Prg22-2(addition of 2no)/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prg22_2_addition_of_2no_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1=Convert.ToInt32(textBox1.Text);
+            int n2=Convert.ToInt32(textBox2.Text);
+            int add = n1 + n2;
+            label3.Text = "Addition of 2 number is : " + add;
+        }
+    }
+}
