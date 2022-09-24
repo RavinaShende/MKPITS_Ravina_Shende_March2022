5edde5ecc1d1a37d41c83b47a7f78e8dd47fd502
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 01:50:43 +0530

    encapsulates properties and provides getter and setter functions.

---
+++ Csharp/Practice Program/Prg17-7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg17_7_encapsulation_
+{
+    class Employee
+    {
+        //string empname;
+        public string empname { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.empname = "ravi"; //this will call setter
+            Console.WriteLine("Employee name : " + emp.empname);//this will call getter
+            Console.ReadLine();
+        }
+    }
+}
