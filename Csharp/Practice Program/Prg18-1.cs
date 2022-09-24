3976b84b64a9e429be2472bd2e1bd0b64caaaf53
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 02:10:42 +0530

    Example of namespace which contains one class "Program".

---
+++ Csharp/Practice Program/Prg18-1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg18_1_namespace_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Namespace!");
+        }
+    }
+}
