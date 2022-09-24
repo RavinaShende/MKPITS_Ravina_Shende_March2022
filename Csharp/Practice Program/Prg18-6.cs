e5b51c3372ab5a39777f58fe573772c908bfd44c
RavinaShende <103641454+RavinaShende@users.noreply.github.com>
2022-09-25 02:23:29 +0530

    example of sealed method

---
+++ Csharp/Practice Program/Prg18-6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg18_6
+{
+    class Person
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("Hello from person");
+        }
+    }
+    class Student : Person
+    {
+        public string name;
+        public sealed override void display()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s= new Student();
+            s.display();
+            Console.ReadLine();
+        }
+    }
+}
