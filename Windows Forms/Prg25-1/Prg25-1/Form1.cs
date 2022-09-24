80a7d9d95638d0f6a46c497ba786e5c72f1e75fc
RavinaShende <ravina.shende794@gmail.com>
2022-09-25 04:39:35 +0530

    windows form app. example on student fetails form

---
+++ Windows Forms/Prg25-1/Prg25-1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prg25_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            student s = new student();
+            s.rno = Convert.ToInt32(textBox1.Text);
+            s.name = textBox2.Text;
+            s.email = textBox3.Text;
+            s.mobileno = Convert.ToInt64(textBox4.Text);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Roll No.: " + s.rno + "\n");
+            sb.Append("Name : " + s.name + "\n");
+            sb.Append("E-Mail : " + s.email + "\n");
+            sb.Append("Mobile no. : " + s.mobileno + "\n");
+            label5.Text = sb.ToString();
+        }
+    }
+}
---
+++ Windows Forms/Prg25-1/Prg25-1/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg25_1
+{
+    internal class student
+    {
+        public int rno;
+        public string name;
+        public string email;
+        public long mobileno;
+    }
+}
